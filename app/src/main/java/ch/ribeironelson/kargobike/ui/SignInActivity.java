134c54e3d43b81bee75ddc95b4918e470bbad314
jonas <jonas.pilloud@students.hevs.ch>
2019-11-30 17:52:36 +0100

    Register + login KargoBike ok

--- app/src/main/java/ch/ribeironelson/kargobike/ui/SignInActivity.java
+++ app/src/main/java/ch/ribeironelson/kargobike/ui/SignInActivity.java
@@ -1,5 +1,7 @@
 package ch.ribeironelson.kargobike.ui;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
@@ -11,13 +13,25 @@ import ch.ribeironelson.kargobike.util.OnAsyncEventListener;
 import ch.ribeironelson.kargobike.viewmodel.UsersListViewModel;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcel;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.internal.firebase_auth.zzew;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUserMetadata;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.auth.zzy;
+import com.google.firebase.auth.zzz;
 
 import java.util.List;
 
@@ -41,6 +55,7 @@ public class SignInActivity extends AppCompatActivity {
 
     private UsersListViewModel usersListViewModel ;
     private List<UserEntity> users;
+    private FirebaseAuth auth;
 
     private UserEntity userToAdd ;
 
@@ -63,6 +78,8 @@ public class SignInActivity extends AppCompatActivity {
 
         registerBtn = findViewById(R.id.register_btn);
         registerBtn.setOnClickListener(v -> verifyUserInputs());
+
+        auth = FirebaseAuth.getInstance() ;
 
         //Get the list of all users to check it
         UsersListViewModel.Factory factory = new UsersListViewModel.Factory(
@@ -96,16 +113,34 @@ public class SignInActivity extends AppCompatActivity {
             UserRepository.getInstance().insert(userToAdd, new OnAsyncEventListener() {
                 @Override
                 public void onSuccess() {
-                    Log.d(TAG, "Insert user : success");
-                    Intent loginIntent = new Intent(SignInActivity.this, LoginActivity.class);
-                    startActivity(loginIntent);
+                    Log.d(TAG, "Insert user in database : success");
                 }
 
                 @Override
                 public void onFailure(Exception e) {
-                    Log.d(TAG, "Insert user : failure");
+                    Log.d(TAG, "Insert user in database : failure");
                 }
             });
+
+            auth.createUserWithEmailAndPassword(userToAdd.getEmail(), password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = auth.getCurrentUser();
+                        Toast.makeText(getApplicationContext(), "User registered successful", Toast.LENGTH_SHORT);
+                        Intent loginAIntent = new Intent(SignInActivity.this, LoginActivity.class);
+                        startActivity(loginAIntent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(getApplicationContext(), "Authentication failed : "+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            });
+
         }if(isUserAlreadyRegister(userToAdd.getEmail())){
             Toast.makeText(this, getString(R.string.error_user_already_exist), Toast.LENGTH_LONG ).show();
         }if(isAnyEditTextEmpty()){
